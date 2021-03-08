abd074ade9c6e6fbe355d01a5fdddaee2fdda2cf
arodriguez133 <arodriguez133@icloud.com>
2021-03-07 20:27:36 -0600

    i'm sure there is a way that you can get task 4 with a one liner. I just ran out of time to figure it out. Task 4 done.

--- index.js
+++ index.js
@@ -243,8 +243,9 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+let centArtists = array.filter(a => a.years > '1900' && a.years < '2000');
+return centArtists.map(n => n.name);
 }
 
 
