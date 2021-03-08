654b89728b42f40d6a33e8740076f9873ce3ff79
arodriguez133 <arodriguez133@icloud.com>
2021-03-07 20:50:01 -0600

    finished task 6 and 7

--- index.js
+++ index.js
@@ -281,9 +281,17 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+function addArtist(array){
+ array.push({
+     "id": 20,
+     "name": "Adan Rodriguez",
+     "years": "1994-present",
+     "genre": "Web Design",
+     "nationality": "The Matrix",
+     "bio": "born in North Carolina, grew up with an affinity towards art and machinery. Was taught how to draw by his mentor, Scott Robertson" 
+   });
+   return array;
+  };
 
   
 
@@ -294,9 +302,10 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
-}
+function lotsOfArt(array){
+ let thatsAlottaPaintings = array.filter(p => p.paintings > 100);
+ return thatsAlottaPaintings.map(n => n.name);
+};
 
 
 
